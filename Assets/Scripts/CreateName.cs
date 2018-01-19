6a82bf9557e4967a23cf3ef41c9c753587cd9ced
Ryan Bates <ryan.bates214@gmail.com>
2018-01-19 17:56:41 -0600

    worked on making a name pop up for each player

---
+++ Assets/Scripts/CreateName.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using UnityEngine;
+
+namespace Ryan
+{
+    public class CreateName : NetworkBehaviour
+    {
+        [SyncVar(hook = "OnSyncMyName")]
+        public string myName;
+
+        void OnSyncMyNameHook(string value)
+        {
+            myName = value;
+            GetComponentInChildren<TextMesh>().text = value;
+        }
+
+        [Command]
+        void CmdSetName(string name)
+        {
+            if (!isLocalPlayer)
+                return;
+
+            myName = name;
+        }
+
+        void ChangeName(string name)
+        {
+            CmdSetName(name);
+        }
+         
+        
+        // Update is called once per frame
+        void Update()
+        {
+        }
+    }
+}
